3313ffe09af870b69beca40dffd71dfd6787fdfb
shishov <sharus.com@gmail.com>
2022-12-25 19:49:49 +0300

    Задача на расчет кредита на мас

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello World!!!');
+const userAge = 25;
+const haveWork = false;
+const cash = 1900;
+const macCost = 2000;
+
+function credit (uage, work){
+	if (uage<24) {
+		return 0;
+	}
+	return (haveWork ? 500 : 100);
+}
+console.log((macCost<=(cash+credit(userAge,haveWork)) ? true : false));
